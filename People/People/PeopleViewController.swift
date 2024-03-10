afbf4f192a1df52e636c11cd06cd4deea11439aa
RomanVakulenko <vakula_48@mail.ru>
2024-03-11 00:38:40 +0300

    checking pull requests

--- People/People/PeopleViewController.swift
+++ People/People/PeopleViewController.swift
@@ -11,7 +11,7 @@
 
         override func viewDidLoad() {
             super.viewDidLoad()
-            // Do any additional setup after loading the view.
+            // checking pull requests
         }
 
 
